4e7a49cb3c44aea786c76efa878be539567cc470
AlEmerich <alan.guitard.pro@gmail.com>
2018-07-05 14:50:23 +0200

    lockeré

--- locker.sh
+++ locker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-exec xautolock -detectsleep 
--time 3 -locker "i3lock -d -c 000070" \
+exec xautolock -detectsleep -time 10 -locker "i3lock -d -i /home/alan/.i3/foxhound.png" \
       -notify 30 \
       -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
