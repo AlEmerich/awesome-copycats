f48722808270a5c83bc08ae1ddd4b6af7e704ffc
AlEmerich <alan.guitard.pro@gmail.com>
2019-01-10 08:53:34 +1100

    change image locking

--- locker.sh
+++ locker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-exec xautolock -detectsleep -time 10 -locker "i3lock -i /home/alan/Images/foxhound.png -p default -d -n" \
+exec xautolock -detectsleep -time 10 -locker "/home/alan/.local/bin/fuzzy_lock.sh " \
       -notify 30 \
       -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
